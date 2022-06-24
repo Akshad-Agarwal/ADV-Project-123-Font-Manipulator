5ba0802f041e0599612644d08c169ec0a00bb395
Akshad-Agarwal <106867353+Akshad-Agarwal@users.noreply.github.com>
2022-06-24 14:29:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+
+}
+
+function setup() {
+    video = createCapture(VIDEO);
+    canvas = createCanvas(600,480);
+    canvas.position(750,180);
+    video.position(50,180);
+    background("#00AEAE");
+}
+
+function draw(){
+
+}
+
